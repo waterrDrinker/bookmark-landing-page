d49ef391f378bb25605212cbfed5296b730f93b1
Grigoriy <grigoriynbr@gmail.com>
2023-07-04 17:43:31 +0300

    Made header desktop

---
+++ src/assets/index.js
@@ -0,0 +1,35 @@
+import dots from './bg-dots.svg';
+import arrow from "./icon-arrow.svg";
+import close from "./icon-close.svg";
+import error from "./icon-error.svg";
+import facebook from "./icon-facebook.svg";
+import twitter from "./icon-twitter.svg";
+import hamburger from "./icon-hamburger.svg";
+import features1 from "./illustration-features-tab-1.svg";
+import features2 from "./illustration-features-tab-2.svg";
+import features3 from "./illustration-features-tab-3.svg";
+import hero from "./illustration-hero.svg";
+import chrome from "./logo-chrome.svg";
+import firefox from "./logo-firefox.svg";
+import opera from "./logo-opera.svg";
+import logo from './logo.svg'
+import logoReversed from './logoReversed.svg'
+
+export {
+	dots,
+	arrow,
+	close,
+	error,
+	facebook,
+	twitter,
+	hamburger,
+	features1,
+	features2,
+	features3,
+	hero,
+	chrome,
+	firefox,
+	opera,
+	logo,
+	logoReversed,
+}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,53 @@
+import { useState } from "react";
 import { navLinks } from '../../constants'
+import { logo, logoReversed } from '../../assets';
+import header from './Header.module.scss';
+import menu from './Menu.module.scss';
 
 const Header = () => {
+	const [active, setActive] = useState('');
+	const [toggle, setToggle] = useState(false)
+
 	return (
-		<header>
-			<div className="">
-				<img src="" alt="" />
+		<header className={`flex container m-auto ${header.header}`}>
+			<div className={header.logo}>
+				<a href="#!"><img src={toggle ? logo : logoReversed} alt="bookmark-logo" /></a>
 			</div>
-			<div className="">
+			<div className={`${header.navbar} flex ai-center uppercase`}>
 				<nav>
-					<ul>
+					<ul className={`flex header-links ${header.links}`}>
 						{navLinks.map((link, index) => (
 							<li key={`link-${index}`}><a href='#!'>{link.title}</a></li>
 						))}
 					</ul>
 				</nav>
-				<a href="#!">Login</a>
+				<a href="#!" className='red-btn'>Login</a>
+			</div>
+			<div className={menu.burger}>
+				<div className={toggle ? `${menu['menu-button-open']}` : `${menu['menu-button']}`}
+					onClick={() => setToggle(!toggle)}>
+					<span className={menu['menu-button-line']}></span>
+					<span className={menu['menu-button-line']}></span>
+					<span className={menu['menu-button-line']}></span>
+				</div>
+				<div className={toggle ? `${menu.mobile}` : 'none'}>
+					<div className={menu.menu}>
+						<div>
+							<nav className={menu["mobile-nav"]}>
+								<ul>
+									{navLinks.map((link, index) => (
+										<li key={`link-${index}`}><a href='#!'>{link.title}</a></li>
+									))}
+								</ul>
+							</nav>
+							<a href="#!" className={menu['login-btn']}>Login</a>
+						</div>
+						<div className="flex">
+							<a href="!#"><i className="facebook-icon"></i></a>
+							<a href="!#"><i className="twitter-icon"></i></a>
+						</div>
+					</div>
+				</div>
 			</div>
 		</header>
 	)
